81cba03dfd2064a290a3fbeb7c7aabdfcfd64a2d
Anina Reinhart <reinharta@outlook.com>
2018-06-05 20:15:05 -0500

    Initial Commit

---
+++ CustomListClass/CustomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    public class CustomList<T>
+    {
+
+        public void Add (T value)
+        {
+
+        }
+
+        public void Remove (T value)
+        {
+
+        }
+
+
+    }
+}
---
+++ CustomListClassTest/CustomListTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CustomListClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListClassTest
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void Add_String_AddValueToList()
+        {
+            //ARRANGE
+            CustomList<string> list = new CustomList<string>();
+
+            //ACT
+
+
+            //ASSERT
+        }
+    }
+}
